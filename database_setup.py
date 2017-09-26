2aa777c963e77e193411b44e524d5743839b8813
Alvin Kang <oneinallin1@gmail.com>
2017-09-26 14:18:53 -0700

    Create sqlite database for user, category, item

---
+++ database_setup.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+	"""Table for users"""
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=False)
+
+
+class Category(Base):
+	"""Table for categories"""
+	__tablename__ = 'category'
+
+	name = Column(String(250), primary_key=True, nullable=False)
+
+
+class Item(Base):
+	"""Table for items"""
+	__tablename__ = 'item'
+
+	id = Column(Integer, primary_key=True)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	category_name = Column(String(250), ForeignKey('category.name'))
+	name = Column(String(250). nullable=False)
+	description = Column(String(250), nullable=False)
+
+
+engine = create_engine('sqlite:///catalog')
+Base.metadata.create_all(engine)
