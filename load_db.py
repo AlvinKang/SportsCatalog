666bead4a83a7da6b7e0f2afa3b5f95ce86e3c21
Alvin Kang <oneinallin1@gmail.com>
2017-09-27 12:57:36 -0700

    Create dummy user

---
+++ load_db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, User, Category, Item
+
+engine = create_engine('sqllite:///catalog.db')
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Create dummy user
+user1 = User(name="Heather Weber", email="heatherweber1983@gmail.com")
+session.add(user1)
+session.commit()
+
