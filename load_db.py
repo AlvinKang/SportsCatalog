31081804c5f65d2d3e4d8385c1a6402773a5c0d0
Alvin Kang <oneinallin1@gmail.com>
2017-09-27 13:03:12 -0700

    Create youth bat item for baseball category

--- load_db.py
+++ load_db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, User, Category, Item
 
-engine = create_engine('sqllite:///catalog.db')
+engine = create_engine("sqllite:///catalog.db")
 
 Base.metadata.bind = engine
 
@@ -15,7 +15,13 @@ user1 = User(name="Heather Weber", email="heatherweber1983@gmail.com")
 session.add(user1)
 session.commit()
 
-# Create category
+# Create baseball category
 category1 = Category(name="Baseball")
 session.add(category1)
+session.commit()
+
+# Create item for baseball
+item1 = Item(user_id=1, category_name="Baseball", name="Youth Bat",
+	description="Youth Bat a good option for players who value the traditional feel of a one-piece bat and the classic ping off a durable aluminum barrel.")
+session.add(item1)
 session.commit()
