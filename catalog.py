444f953bbdedde3abf1c46d38eb4b29d357d5015
Alvin Kang <oneinallin1@gmail.com>
2017-09-29 14:57:37 -0700

    Add route and function for create new item page

--- catalog.py
+++ catalog.py
@@ -13,25 +13,47 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 #########################################
+# HOME PAGE
 
 # Show all categories (main page)
 @app.route('/')
 @app.route('/catalog/')
 def showCategories():
-	categories = session.query(Category)
+	categories = session.query(Category).all()
 	return render_template('categories.html', categories=categories)
 
 #########################################
+# CATEGORY ITEMS PAGE
 
 # Show items of category
 @app.route('/catalog/<category_name>/')
-@app.route('/catalog/<category_name>/items/')
 def showItems(category_name):
 	items = session.query(Item).filter_by(category_name=category_name).all()
 	for item in items:
 		print item.name
 		print category_name
 	return render_template('items.html', category_name=category_name, items=items)
+
+# Create a new item
+@app.route('/catalog/new/', methods=['GET', 'POST'])
+def newItem():
+	if request.method == 'POST':
+		newItem = Item(user_id=1, category_name=request.form['category'], name=request.form['itemName'], description=request.form['itemDescription'])
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('showCategories'))
+	else:
+		return render_template('newItem.html')
+
+
+#########################################
+# ITEM PAGE
+
+# Show item info
+@app.route('/catalog/<category_name>/items/<item_name>/')
+def showItemInfo(category_name, item_name):
+	item = session.query(Item).filter_by(category_name=category_name, name=item_name).one()
+	return render_template('itemInfo.html', item=item)
 
 
 if __name__ == '__main__':
