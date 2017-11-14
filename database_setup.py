38c498842ea4ed26ea826def7353cd3890c3c8ef
Alvin Kang <oneinallin1@gmail.com>
2017-11-14 14:18:20 -0800

    Clean up to follow PEP-8

--- database_setup.py
+++ database_setup.py
@@ -1,53 +1,54 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 
 class User(Base):
-	"""Table for users"""
-	__tablename__ = 'user'
+    """Table for users"""
+    __tablename__ = 'user'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	email = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
 
 class Category(Base):
-	"""Table for categories"""
-	__tablename__ = 'category'
+    """Table for categories"""
+    __tablename__ = 'category'
 
-	name = Column(String(250), primary_key=True, nullable=False)
+    name = Column(String(250), primary_key=True, nullable=False)
 
-	@property
-	def serialize(self):
-		'''Return object data in easily serializeable format'''
-		return {
-			'name': self.name
-		}
+    @property
+    def serialize(self):
+        '''Return object data in easily serializeable format'''
+        return {
+            'name': self.name
+        }
 
 
 class Item(Base):
-	"""Table for items"""
-	__tablename__ = 'item'
+    """Table for items"""
+    __tablename__ = 'item'
 
-	id = Column(Integer, primary_key=True)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	category_name = Column(String(250), ForeignKey('category.name'))
-	name = Column(String(250), nullable=False)
-	description = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    category_name = Column(String(250), ForeignKey('category.name'))
+    category = relationship(Category)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
 
-	@property
-	def serialize(self):
-		'''Return object data in easily serializeable format'''
-		return {
-			'name': self.name,
-			'id': self.id,
-			'category_name': self.category_name,
-			'description': self.description
-		}
+    @property
+    def serialize(self):
+        '''Return object data in easily serializeable format'''
+        return {
+            'name': self.name,
+            'id': self.id,
+            'category_name': self.category_name,
+            'description': self.description
+        }
 
 
 engine = create_engine('sqlite:///catalog.db')
